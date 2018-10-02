6d7a9e5106da2bc416444d4bf60921980ec41f0b
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-02 12:05:26 -0500

    init project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const config = {
+  devServer: {
+    compress: true,
+    hot: true,
+    port: 8000,
+    historyApiFallback: true,
+    open: true,
+    watchOptions: {
+      ignored: /node_modules/
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', {
+          loader: 'css-loader',
+          options: {
+            modules: false
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+module.exports = config
