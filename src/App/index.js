53671dc5667e77f6604bf284fd9e9fab30c2111b
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-02 20:47:01 -0500

    Added events to change search

--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import './App.css'
+import SearchInput from '../components/SearchInput'
 export default class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      title: 'Pokedex'
+      title: 'Pokedex',
+      search: ''
     }
+    this.onChangeSearch = this.onChangeSearch.bind(this)
+  }
+
+  onChangeSearch (event) {
+    this.setState({
+      search: event.target.value
+    })
   }
   render () {
     return (
       <Layout classes='Full-Page'>
         <h1>{this.state.title}</h1>
+        <SearchInput value={this.state.search} onChange={this.onChangeSearch} />
       </Layout>
     )
   }
