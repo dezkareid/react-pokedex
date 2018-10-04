37c6f7af192bbba101c5c954ea0b36307d30acf5
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-03 21:41:33 -0500

    removed li element

--- src/components/PokeItem/index.js
+++ src/components/PokeItem/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 export default function PokeItem (props) {
   return (
-    <li>
-      <figure>
-        <img src={props.avatar} />
-        <figcaption>
-          <span>{props.name}</span>
-        </figcaption>
-      </figure>
-    </li>
+    <figure>
+      <img src={props.avatar} />
+      <figcaption>
+        <span>{props.name}</span>
+      </figcaption>
+    </figure>
   )
 }
