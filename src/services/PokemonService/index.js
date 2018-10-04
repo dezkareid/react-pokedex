78a2bfc237ef5d92634721594d53a4882f568484
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-03 19:48:14 -0500

    Added avatar

--- src/services/PokemonService/index.js
+++ src/services/PokemonService/index.js
@@ -1,5 +1,5 @@
 const pokemons = [
-  { id: 1, name: 'Bulbasaur' }
+  { id: 1, name: 'Bulbasaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' }
 ]
 export function getPokemons () {
   return Promise.resolve(pokemons)
