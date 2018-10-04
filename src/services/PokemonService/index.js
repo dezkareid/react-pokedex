e7250ef0553e8ea257d4287888870fa1aa943972
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-03 19:59:33 -0500

    Added Ivysaur

--- src/services/PokemonService/index.js
+++ src/services/PokemonService/index.js
@@ -1,5 +1,6 @@
 const pokemons = [
-  { id: 1, name: 'Bulbasaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' }
+  { id: 1, name: 'Bulbasaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' },
+  { id: 2, name: 'Ivysaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' }
 ]
 export function getPokemons () {
   return Promise.resolve(pokemons)
