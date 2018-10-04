9dfda42c09d0888ad130c1b9cd67f8bc4182790e
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-03 21:01:20 -0500

    added debouncing

--- src/App/index.js
+++ src/App/index.js
@@ -6,6 +6,7 @@ import ErrorBoundary from '../components/ErrorBoundary'
 import ErrorCharge from '../components/ErrorCharge'
 import PokeList from '../components/PokeList'
 import { getPokemons } from '../services/PokemonService'
+import debounce from 'lodash.debounce'
 export default class App extends React.Component {
   constructor (props) {
     super(props)
@@ -15,25 +16,34 @@ export default class App extends React.Component {
       pokemons: []
     }
     this.onChangeSearch = this.onChangeSearch.bind(this)
+    this.fetchPokemons = debounce(this.fetchPokemons, 500)
   }
 
   onChangeSearch (event) {
+    const search = event.target.value
     this.setState({
-      search: event.target.value
+      search
     })
+    this.fetchPokemons(search)
   }
 
   componentDidMount () {
-    getPokemons()
+    this.fetchPokemons('')
+  }
+
+  fetchPokemons (filter) {
+    console.log('fetching pokemons')
+    getPokemons(filter)
       .then((pokemons) => { this.setState({ pokemons }) })
   }
+
   render () {
     return (
       <Layout classes='Full-Page'>
         <ErrorBoundary errorComponent={ErrorCharge}>
           <h1>{this.state.title}</h1>
           <SearchInput value={this.state.search} onChange={this.onChangeSearch} />
-          <PokeList pokemons={this.state.pokemons} filter={this.state.search} />
+          <PokeList pokemons={this.state.pokemons} />
         </ErrorBoundary>
       </Layout>
     )
