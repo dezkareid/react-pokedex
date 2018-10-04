74e9c53b868b112dc59b700118feaf7299198216
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-03 20:00:00 -0500

    Added filter pokemons

--- src/components/PokeList/index.js
+++ src/components/PokeList/index.js
@@ -3,7 +3,8 @@ import PokeItem from '../PokeItem'
 import './PokeList.css'
 
 export default function PokeList (props) {
-  const listPokemons = props.pokemons.map((pokemon) => <PokeItem key={pokemon.id} {...pokemon} />)
+  const pokemons = props.filter ? props.pokemons.filter((pokemon) => pokemon.name.includes(props.filter)) : props.pokemons
+  const listPokemons = pokemons.map((pokemon) => <PokeItem key={pokemon.id} {...pokemon} />)
   return (
     <ul className='PokeList'>
       {listPokemons}
