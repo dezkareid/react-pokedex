bd3dc8881d348616db13ef89418b92aee1903b3f
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-08 16:28:24 -0500

    Added link

--- src/components/PokeListItem/index.js
+++ src/components/PokeListItem/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function PokeListItem (props) {
   const { pokemon, PokeComponent } = props
   return (
     <li onClick={() => props.onClickPokemon(pokemon)}>
-      <PokeComponent {...pokemon} />
+      <Link to={`pokemons/${pokemon.id}`}>
+        <PokeComponent {...pokemon} />
+      </Link>
     </li>
   )
 }
