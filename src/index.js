4f6c509f8ba87960179698de1e5c77f1f16211df
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-08 16:28:02 -0500

    component appwithrouter instead app component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './AppWithRouter'
 
 const appElement = document.getElementById('app-element')
 
