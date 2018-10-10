8747536dabef8fbeac34f40efc152dd403baa3f3
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-09 19:52:45 -0500

    Added chart to pokemon page

--- src/pages/PokemonPage/index.js
+++ src/pages/PokemonPage/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import { Link } from 'react-router-dom'
+import PokeChart from '../../components/PokeChart'
 export default class PokemonPage extends React.Component {
   render () {
     return (
       <Layout>
         <Link to='/'>Return home</Link>
-        <p>Hello</p>
+        <PokeChart />
       </Layout>
     )
   }
