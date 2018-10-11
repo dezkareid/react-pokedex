f4f7a1bb3d0ea6f6a29787ae3e5ffda3b77ec862
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-10 21:03:11 -0500

    Added fixture to pokemon list

--- test/components/PokeList/pokelist-test.js
+++ test/components/PokeList/pokelist-test.js
@@ -1,11 +1,11 @@
 /* eslint-env jest */
 import { shallow } from 'enzyme'
 import PokeList from '../../../src/components/PokeList'
+import { emptyPokemons } from '../../fixtures/fake-pokemons'
 
 describe('PokeList suite', () => {
-  test('When the prop name has 3 items PokeList must has 2 childrens', () => {
-    const pokemons = [{ id: 1, name: '', avatar: '' }, { id: 2, name: '', avatar: '' }, { id: 3, name: '', avatar: '' }]
-    const wrapper = shallow(<PokeList pokemons={pokemons} />)
-    expect(wrapper.children()).toHaveLength(3)
+  test('When the prop pokemons has N items PokeList must has N childrens', () => {
+    const wrapper = shallow(<PokeList pokemons={emptyPokemons} />)
+    expect(wrapper.children()).toHaveLength(emptyPokemons.length)
   })
 })
