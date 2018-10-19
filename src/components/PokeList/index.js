5149492d98850c95ef54f392dcd674b3a5db317e
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-18 19:45:06 -0500

    Added animation

--- src/components/PokeList/index.js
+++ src/components/PokeList/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import './PokeList.css'
+import { CSSTransitionGroup } from 'react-transition-group'
+import './PokeList.scss'
 import PokeListItem from '../PokeListItem'
 
 export default function PokeList (props) {
@@ -7,7 +8,14 @@ export default function PokeList (props) {
   const listPokemons = pokemons.map((pokemon) => <PokeListItem key={pokemon.id} pokemon={pokemon} {...restProperties} />)
   return (
     <ul className='PokeList'>
-      {listPokemons}
+      <CSSTransitionGroup
+        transitionName='PokeList'
+        transitionAppear
+        transitionAppearTimeout={500}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+        {listPokemons}
+      </CSSTransitionGroup>
     </ul>
   )
 }
