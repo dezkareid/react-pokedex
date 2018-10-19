8d5b15460ebb632de797816564d08eb45c92ae78
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-18 20:22:29 -0500

    Added auth with firebase

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,10 +4,13 @@ import PokeList from '../../components/PokeList'
 import { getPokemons } from '../../services/PokemonService'
 import debounce from 'lodash.debounce'
 import PokeItem from '../../components/PokeItem'
+import FirebaseAdapter from '../../utils/FirebaseAdapter'
+const auth = FirebaseAdapter.getAuth()
 export default class HomePage extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      auth: false,
       title: 'Pokedex',
       search: '',
       pokemons: []
@@ -18,6 +21,14 @@ export default class HomePage extends React.Component {
       const pokemon = { id: 3, name: 'Venasaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' }
       this.addPokemon(pokemon)
     }, 5000)
+    this.authenticate = this.authenticate.bind(this)
+    auth().onAuthStateChanged((auth) => {
+      if (auth) {
+        this.setState({ auth })
+      } else {
+        this.setState({ auth: false })
+      }
+    })
   }
 
   addPokemon (pokemon) {
@@ -26,6 +37,11 @@ export default class HomePage extends React.Component {
         pokemons: [ ...prevState.pokemons, pokemon ]
       }
     })
+  }
+
+  authenticate () {
+    const provider = new auth.FacebookAuthProvider()
+    auth().signInWithPopup(provider)
   }
 
   onChangeSearch (event) {
@@ -41,7 +57,6 @@ export default class HomePage extends React.Component {
   }
 
   fetchPokemons (filter) {
-    console.log('fetching pokemons')
     getPokemons(filter)
       .then((pokemons) => { this.setState({ pokemons }) })
   }
@@ -54,6 +69,7 @@ export default class HomePage extends React.Component {
     return (
       <React.Fragment>
         <h1>{this.state.title}</h1>
+        { this.state.auth ? null : <button onClick={this.authenticate}>Identificate</button>}
         <SearchInput value={this.state.search} onChange={this.onChangeSearch} />
         <PokeList pokemons={this.state.pokemons} onClickPokemon={this.onClickPokemon} PokeComponent={PokeItem} />
       </React.Fragment>
