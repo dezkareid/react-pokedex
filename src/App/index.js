3c08539111c992d0ea51b30f7fefcf7b1fdc3b84
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-19 20:15:00 -0500

    Added provider

--- src/App/index.js
+++ src/App/index.js
@@ -3,19 +3,37 @@ import Routes from '../Routes'
 import Layout from '../components/Layout'
 import ErrorBoundary from '../components/ErrorBoundary'
 import ErrorCharge from '../components/ErrorCharge'
+import Context from '../AppContext'
+import FirebaseAdapter from '../utils/FirebaseAdapter'
 import './App.css'
 export default class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      auth: false
+    }
+    this.auth = FirebaseAdapter.getAuth()
+    this.unsuscribe = this.auth().onAuthStateChanged((auth) => {
+      if (auth) {
+        this.setState({ auth })
+      } else {
+        this.setState({ auth: false })
+      }
+    })
+  }
+
+  componentWillUnmount () {
+    this.unsuscribe && this.unsuscribe()
   }
 
   render () {
     return (
       <ErrorBoundary errorComponent={ErrorCharge}>
-        <Layout classes='Full-Page'>
-          <Routes />
-        </Layout>
+        <Context.Provider value={this.state}>
+          <Layout classes='Full-Page'>
+            <Routes />
+          </Layout>
+        </Context.Provider>
       </ErrorBoundary>
     )
   }
