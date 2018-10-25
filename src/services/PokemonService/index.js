f1c0318f6e4c72634fc97acbb40fbbf403f3f249
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-24 21:22:57 -0500

    updated getPokemons

--- src/services/PokemonService/index.js
+++ src/services/PokemonService/index.js
@@ -1,8 +1,7 @@
-const pokemons = [
-  { id: 1, name: 'Bulbasaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' },
-  { id: 2, name: 'Ivysaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' }
-]
-export function getPokemons (filter = '') {
-  const filteredPokemons = filter ? pokemons.filter((pokemon) => pokemon.name.includes(filter)) : pokemons
-  return Promise.resolve(filteredPokemons)
+const pokemons = {
+  '1': { id: 1, name: 'Bulbasaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' },
+  '2': { id: 2, name: 'Ivysaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' }
+}
+export function getPokemons () {
+  return Promise.resolve(pokemons)
 }
