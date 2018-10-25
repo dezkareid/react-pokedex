7276a07662fbf7ccc101c7bbcc08b9d6fe313b8d
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-24 21:24:19 -0500

    added async action

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -3,6 +3,17 @@ import {
   ADD_POKEMON
 } from './action-names'
 import actionCreator from './action-creator'
+import { getPokemons } from '../../services/PokemonService'
 export const addPokemons = actionCreator.bind(null, ADD_POKEMONS)
 
 export const addPokemon = actionCreator.bind(null, ADD_POKEMON)
+
+export function fetchPokemons () {
+  return (dispatch) => {
+    return getPokemons()
+      .then(pokemons => {
+        dispatch(addPokemons({pokemons}))
+      })
+      .catch(console.error)
+  }
+}
