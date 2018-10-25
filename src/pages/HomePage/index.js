0bd127ce6b347eb26dbe21fe124d9943e4c5923a
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-24 21:25:03 -0500

    connected with redux

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,35 +1,18 @@
 import React from 'react'
-import SearchInput from '../../components/SearchInput'
 import PokeList from '../../components/PokeList'
-import { getPokemons } from '../../services/PokemonService'
-import debounce from 'lodash.debounce'
 import PokeItem from '../../components/PokeItem'
 import FirebaseAdapter from '../../utils/FirebaseAdapter'
 import Context from '../../AppContext'
+import { fetchPokemons } from '../../state/actions'
+import { connect } from 'react-redux'
 const auth = FirebaseAdapter.getAuth()
 export class HomePage extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      title: 'Pokedex',
-      search: '',
-      pokemons: []
+      title: 'Pokedex'
     }
-    this.onChangeSearch = this.onChangeSearch.bind(this)
-    this.fetchPokemons = debounce(this.fetchPokemons, 500)
-    setTimeout(() => {
-      const pokemon = { id: 3, name: 'Venasaur', avatar: 'https://vignette.wikia.nocookie.net/es.pokemon/images/9/9a/Bulbasaur_%28anime_AG%29.png/revision/latest?cb=20120906022549' }
-      this.addPokemon(pokemon)
-    }, 1000)
     this.authenticate = this.authenticate.bind(this)
-  }
-
-  addPokemon (pokemon) {
-    this.setState((prevState) => {
-      return {
-        pokemons: [ ...prevState.pokemons, pokemon ]
-      }
-    })
   }
 
   authenticate () {
@@ -37,21 +20,8 @@ export class HomePage extends React.Component {
     auth().signInWithPopup(provider)
   }
 
-  onChangeSearch (event) {
-    const search = event.target.value
-    this.setState({
-      search
-    })
-    this.fetchPokemons(search)
-  }
-
   componentDidMount () {
-    this.fetchPokemons('')
-  }
-
-  fetchPokemons (filter) {
-    getPokemons(filter)
-      .then((pokemons) => { this.setState({ pokemons }) })
+    this.props.fetchPokemons()
   }
 
   onClickPokemon (pokemon) {
@@ -63,17 +33,22 @@ export class HomePage extends React.Component {
       <React.Fragment>
         <h1>{this.state.title}</h1>
         { this.props.auth ? null : <button onClick={this.authenticate}>Identificate</button>}
-        <SearchInput value={this.state.search} onChange={this.onChangeSearch} />
-        <PokeList pokemons={this.state.pokemons} onClickPokemon={this.onClickPokemon} PokeComponent={PokeItem} />
+        <PokeList onClickPokemon={this.onClickPokemon} PokeComponent={PokeItem} />
       </React.Fragment>
     )
   }
 }
+function mapDispatchToProps (dispatch) {
+  return {
+    fetchPokemons: () => dispatch(fetchPokemons())
+  }
+}
 
 export default () => {
+  const HomeComponent = connect(null, mapDispatchToProps)(HomePage)
   return (
     <Context.Consumer>
-      { (value) => <HomePage {...value} />}
+      { (value) => <HomeComponent {...value} />}
     </Context.Consumer>
   )
 }
