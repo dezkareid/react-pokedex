3d2388736a46a4783ffa0cca143c0f6a30eb12d3
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-24 21:25:23 -0500

    added provider

--- src/AppWithRouter/index.js
+++ src/AppWithRouter/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import Store from '../state/store'
 import App from '../App'
+
+console.log(Store.getState())
 export default function AppWithRouter (props) {
   return (
     <BrowserRouter>
-      <App />
+      <Provider store={Store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   )
 }
